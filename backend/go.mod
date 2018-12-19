e424221443b05a00a4001c20ebabfcbddc74102f
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-12-18 19:39:23 -0500

    Create hello world go backend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/jbrunsting/transient/backend
+
+require github.com/gorilla/mux v1.6.2
---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello world!"}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+
+	fmt.Println("Listening on port 3000")
+	http.ListenAndServe(":3000", r)
+}
