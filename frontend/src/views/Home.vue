c8c4d8b2e8226a1d92c890d8cd2c414fe9056c88
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-12-21 20:32:40 -0500

    Make frontend make testing to backend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h2>{{ data }}</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -11,8 +12,27 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
     name: 'home',
+    data() {
+        return {
+            data: '',
+        };
+    },
     components: {
         HelloWorld,
+    },
+    methods: {
+        mounted() {
+            console.log('HI');
+        },
+    },
+    created() {
+        this.$http.get('/api/')
+            .then((response) => {
+                this.data = response.data.message;
+            }).catch((e) => {
+                console.log(`Error ${JSON.stringify(e)}`);
+            });
+        this.data = 'test';
     },
 };
 </script>
