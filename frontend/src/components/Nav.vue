c2b354a2dc0a17f5cc7669e8daa5fe507ffbf53d
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-12-25 14:13:57 -0500

    Create nav component

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+};
+</script>
+
+<style scoped lang="scss">
+.nav {
+  a {
+    padding: 30px;
+    font-weight: bold;
+    &.router-link-exact-active {
+      color: red;
+    }
+  }
+}
+</style>
