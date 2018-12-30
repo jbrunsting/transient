18e930aa7fa70098a976dc409c71e59d91ef48a4
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-12-29 21:58:54 -0500

    Move signup into home page

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -3,14 +3,12 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
     <Signin v-on:signin="updateAuth()"/>
-    <Signup v-if="!authenticated" v-on:signup="updateAuth()"/>
     <Signout v-if="authenticated" v-on:signout="updateAuth()"/>
   </div>
 </template>
 
 <script>
 import Signin from '@/components/Signin.vue';
-import Signup from '@/components/Signup.vue';
 import Signout from '@/components/Signout.vue';
 
 export default {
@@ -20,7 +18,6 @@ export default {
     },
     components: {
         Signin,
-        Signup,
         Signout,
     },
     methods: {
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Nav v-on:auth="updateAuth()" :authenticated="authenticated"/>
+    <Signup v-if="!authenticated" v-on:signup="updateAuth()"/>
     <img alt="Vue logo" src="../assets/logo.png">
     <h2>{{ data }}</h2>
     <h2>{{ authenticated }}</h2>
@@ -11,6 +12,7 @@
 <script>
 import HelloWorld from '@/components/HelloWorld.vue';
 import Nav from '@/components/Nav.vue';
+import Signup from '@/components/Signup.vue';
 
 export default {
     name: 'home',
@@ -25,6 +27,7 @@ export default {
     components: {
         HelloWorld,
         Nav,
+        Signup,
     },
     methods: {
         mounted() {
