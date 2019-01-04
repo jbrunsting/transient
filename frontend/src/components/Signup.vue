8322648abf24d0297ccaa1bb756e733869b8bd4f
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-01-03 21:15:38 -0500

    Add error tooltips to signup component

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="signup">
+    <Error direction="right" class="uniqueness error">
+      That username or email is already taken
+    </Error>
+    <Error direction="right" class="format error">
+      Username or email format incorrect
+    </Error>
+    <Error direction="right" class="unknown error">
+      Could not sign up, please try again later
+    </Error>
     <form @submit.prevent="signup">
       <input placeholder="username" v-model="username">
       <input type="email" placeholder="email" v-model="email">
@@ -11,6 +20,8 @@
 </template>
 
 <script>
+import Error from '@/components/Error.vue';
+
 export default {
     name: 'Signup',
     data() {
@@ -21,20 +32,35 @@ export default {
             response: '',
         };
     },
+    components: {
+        Error,
+    },
     methods: {
         signup() {
+            /* eslint-disable no-param-reassign */
+            this.$el.querySelectorAll('.error').forEach((c) => {
+                c.style.display = 'none';
+            });
+
             const user = {
                 username: this.username,
                 email: this.email,
                 password: this.password,
             };
             this.$http.post('/api/user', user)
-                .then((response) => {
-                    console.log(`Error ${JSON.stringify(response)}`);
+                .then(() => {
                     this.$emit('signup');
                 }).catch((e) => {
-                    console.log(`Error ${JSON.stringify(e)}`);
+                    if (e.response.data.kind === this.UNIQUENESS_VIOLATION) {
+                        this.$el.querySelector('.uniqueness.error').style.display = 'inline-block';
+                    } else if (e.response.data.kind === this.DATA_VIOLATION) {
+                        this.$el.querySelector('.format.error').style.display = 'inline-block';
+                    } else {
+                        console.log(`Error: ${e.response.data.message}`);
+                        this.$el.querySelector('.unknown.error').style.display = 'inline-block';
+                    }
                 });
+            /* eslint-enable no-param-reassign */
         },
     },
 };
@@ -42,6 +68,10 @@ export default {
 
 <style scoped lang="scss">
 @import "../styles/settings.scss";
+
+.signup {
+  position: relative;
+}
 
 input {
   margin-right: 0;
@@ -52,5 +82,12 @@ form {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+}
+
+.error {
+  display: none;
+  position: absolute;
+  width: 180px;
+  right: 180px;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,11 @@ import router from './router';
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 
-Vue.prototype.CONNECTION = "connection"
-Vue.prototype.NOT_FOUND = "not_found"
-Vue.prototype.DATA_VIOLATION = "data_volation"
-Vue.prototype.UNIQUENESS_VIOLATION = "uniqueness_violation"
-Vue.prototype.UNEXPECTED = "unexpected"
+Vue.prototype.CONNECTION = 'connection'
+Vue.prototype.NOT_FOUND = 'not_found'
+Vue.prototype.DATA_VIOLATION = 'data_volation'
+Vue.prototype.UNIQUENESS_VIOLATION = 'uniqueness_violation'
+Vue.prototype.UNEXPECTED = 'unexpected'
 
 new Vue({
     router,
